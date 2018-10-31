b778caca03d4d11dd627860a0feb79b3991de11b
Jose Alejandro Jimenez Rosa <jose.jimenez@inabima.gob.do>
2018-10-31 10:22:32 -0400

    continuacion de programas

--- src/index.js
+++ src/index.js
@@ -63,13 +63,23 @@ function getTurnData(authors) {
 }
 
 const state = {
-    turnData: getTurnData(authors)
+	turnData: getTurnData(authors),
+	highlight: ''
+	
 };
 
+function onAnswerSelected(answer){
+	const isCorrect = state.turnData.author.books.some((book) => book === answer);
+	state.highlight = isCorrect ? 'correct' : 'wrong';
+	render();
+}
 
-ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
-
+function render()
+{
+ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+render();
 serviceWorker.unregister();
